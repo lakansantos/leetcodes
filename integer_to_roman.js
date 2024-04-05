55e0aecc8b3cc36ca6918224875aae6b634a7dcd
lakansantos <rizesuoracle25@gmail.com>
2024-04-05 12:54:37 +0800

    Added integer to roman

---
+++ integer_to_roman.js
@@ -0,0 +1,27 @@
+var intToRoman = function (num) {
+  if (num === 0) return "";
+  const symbols = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  const labelSymbol = Object.keys(symbols).find(
+    (symbol) => symbols[symbol] <= num
+  );
+
+  const valueSymbol = symbols[labelSymbol];
+  return labelSymbol + intToRoman(num - valueSymbol);
+};
+
+console.log(intToRoman(1700)); // Output: MDCC
