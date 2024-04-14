c58662eb390113c1941edb566bfcc40ff6d8e1ae
lakansantos <rizesuoracle25@gmail.com>
2024-04-15 07:59:04 +0800

    Simplified uppercase first letter function

--- uppercase_first_letter.js
+++ uppercase_first_letter.js
@@ -1,15 +1,9 @@
 function capitalizeFirstLetter(sentence) {
-  sentence.trim(); // trim the  sentence
-
-  let result = "";
-  [sentence].forEach((element) => {
-    // make it an array to be iterated
-    element.split(" ").map((el) => {
-      // split each element then iterate then get the first word of each splitted element
-      result += el[0].toUpperCase() + el.slice(1, el.length) + " ";
-    });
-  });
-  return result;
+  return sentence
+    .trim()
+    .split(/\s+/) // removes whitespaces
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
-console.log(capitalizeFirstLetter("this is a sentence"));
+console.log(capitalizeFirstLetter("this is a      sentence"));
