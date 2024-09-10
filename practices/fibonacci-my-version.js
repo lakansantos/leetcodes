b37e48a0fc7019d984c6ebc3ed12789d391968c8
lakansantos <rizesuoracle25@gmail.com>
2024-09-10 10:13:55 +0800

    fixed fibonacci

--- practices/fibonacci-my-version.js
+++ practices/fibonacci-my-version.js
@@ -4,6 +4,8 @@ const fibonacci = (starting, length) => {
   let leftIndex = rightIndex - 1;
 
   let total = 0;
+
+  if (starting.length >= length) return starting;
   while (starting.length !== length) {
     const leftValue = starting[leftIndex];
     const rightValue = starting[rightIndex];
@@ -18,7 +20,10 @@ const fibonacci = (starting, length) => {
   return starting;
 };
 
-console.log(fibonacci([1], 10));
+console.log(fibonacci([3, 3], 1)); // return the same
+console.log(fibonacci([3, 3], 2)); // return the same
+console.log(fibonacci([1, 7], 3)); // [ 1, 7, 8 ]
+console.log(fibonacci([1, 1], 5)); // [ 1, 1, 2, 3, 5 ]
 
 // 1 2 = 3
 // 1 2 3 = 5
